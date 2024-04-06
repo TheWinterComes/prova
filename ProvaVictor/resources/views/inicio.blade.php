f1c0ad80f9b74531c15101b52b20afe4e02d014e
Victor Correia <128752738+TheWinterComes@users.noreply.github.com>
2024-04-06 00:10:47 +0000

    Prova

---
+++ ProvaVictor/resources/views/inicio.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<div class="alert alert-primary" role="alert">
+  Olá!
+</div>
+<div class="alert alert-secondary" role="alert">
+  Olá!
+</div>
+<div class="alert alert-success" role="alert">
+  Olá!
+</div>
+<div class="alert alert-danger" role="alert">
+  Olá!
+</div>
+<div class="alert alert-warning" role="alert">
+  Olá!
+</div>
+<div class="alert alert-info" role="alert">
+  Olá!
+</div>
+<div class="alert alert-light" role="alert">
+  Olá!
+</div>
+<div class="alert alert-dark" role="alert">
+  Olá!
+</div>
+
+</body>
+</html>
